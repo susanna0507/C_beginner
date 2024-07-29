ebdffa54e8f4b75e82a6f1eb444832d98924347d
younsu <susanna10507@gmail.com>
2024-07-29 18:23:57 +0900

    Day1
    PJT 생성, 변수, 출력방법 등

---
+++ PJT02_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT02_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            int result;
+
+            a = 100;
+            b = 50;
+
+            result = a + b;
+            Console.WriteLine(a + "+" + b + "=" + result); // 100+50=150
+            result = a - b;
+            Console.WriteLine(a + "-" + b + "=" + result); // 100-50=50
+            result = a * b;
+            Console.WriteLine(a + "*" + b + "=" + result); // 100*50=5000
+            result = a / b;
+            Console.WriteLine(a + "/" + b + "=" + result); // 100/50=2
+        }
+    }
+}
---
+++ PJT02_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJT02_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b; // 변수
+            int result;
+
+            string str;
+
+            Console.Write("첫번째 계한할 값을 입력하세요. : ");
+            str = Console.ReadLine();  // 키보드값 입력 받음(단, 문자열로만 받음)
+            a = Convert.ToInt32(str);  // 문자열을 숫자로 변환
+
+            Console.Write("두번째 계한할 값을 입력하세요. : ");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+
+            result = a + b;
+            Console.WriteLine(a + "+" + b + "=" + result);
+        }
+    }
+}
